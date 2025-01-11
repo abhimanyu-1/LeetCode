cbb36cf55a9e935eab943838194ffa48fa71dae6
abhimanyu <116670401+abhimanyu-1@users.noreply.github.com>
2025-01-11 11:50:12 +0530

    Time: 30 ms (17.99%), Space: 45.5 MB (32.37%) - LeetHub

--- 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.java
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.java
@@ -3,19 +3,16 @@ class Solution {
     {
         if(s.length()<k){return false;}
         Map<Character,Integer> freequency = new HashMap<>();
-        HashSet<Character> unq = new HashSet<>();
         int oddCount = 0 ;
-        int evenCount = 0;
         char[] chars = s.toCharArray();
 
         for(char c:chars)
         {
-            unq.add(c);
             freequency.put(c,freequency.getOrDefault(c,0)+1);
         }
-        for(char c:unq)
+        for(int c:freequency.values())
         {
-            if(freequency.get(c)%2 != 0)
+            if(c%2 != 0)
             {
                 oddCount++;
             }
